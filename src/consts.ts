f60c30709a037e76d86db2c6a9f0d661c998849b
Swapnil Sharma <swap.sha96@gmail.com>
2025-04-12 21:28:33 +0530

    feat: add About page with host profiles and updated site metadata for improved SEO

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,26 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Lemonade Stand Podcast';
-export const SITE_DESCRIPTION = 'New takes on Business, Tech, and Politics. Squeezed fresh every Thursday.';
-export const SITE_SCHEDULE = 'Every Thursday';
+export const SITE_DESCRIPTION = 'Three guys with the expertise to run a lemonade stand explain the world of business. New episodes every Thursday.';
+export const SITE_TAGLINE = 'A podcast about business, tech, and politics by @aidencalvin, @Atrioc, and @DougDougFood';
+export const SITE_SCHEDULE = 'Thursday';
 export const SITE_URL = 'https://lemonadestandpodcast.github.io';
+
+export const HOSTS = {
+    aiden: {
+        name: 'Aiden Calvin',
+        twitter: '@aidencalvin'
+    },
+    atrioc: {
+        name: 'Brandon "Atrioc" Ewing',
+        twitter: '@Atrioc'
+    },
+    dougdoug: {
+        name: 'Douglas "DougDoug" Wreden',
+        twitter: '@DougDougFood'
+    }
+};
+
 export const SOCIAL_LINKS = {
     github: 'https://github.com/lemonadestandpodcast/lemonadestandpodcast.github.io',
     twitter: 'https://twitter.com' // TODO: Add actual Twitter link
